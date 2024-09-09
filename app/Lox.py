ffa99f99f08482dd9b863ff3256bee6883107430
Yevgeniy Ustinov <ev.ustinov03@gmail.com>
2024-09-09 18:38:13 +0200

    interpreter

--- app/Lox.py
+++ app/Lox.py
@@ -87,7 +87,7 @@ class Lox:
 
     @staticmethod
     def runtime_error(error: RuntimeException) -> None:
-        print(f"{error}\n[line {error.token.line}]")
+        print(f"\n{error} [line {error.token.line}]")
         Lox.had_runtime_error = True
 
 
